bbd88c5dbf7ac18de7be289dc0bacf9911a6dd4b
Kayla Son <ksonkayla@gmail.com>
2023-02-17 21:11:27 -0800

    part 4

--- client/src/app/pages/album-page/album-page.component.ts
+++ client/src/app/pages/album-page/album-page.component.ts
@@ -3,6 +3,7 @@ import { ActivatedRoute } from '@angular/router';
 import { ArtistData } from '../../data/artist-data';
 import { TrackData } from '../../data/track-data';
 import { AlbumData } from '../../data/album-data';
+import { SpotifyService } from '../../services/spotify.service';
 
 @Component({
   selector: 'app-album-page',
@@ -15,11 +16,13 @@ export class AlbumPageComponent implements OnInit {
 	tracks:TrackData[];
 
 
-  constructor(private route: ActivatedRoute) { }
+  constructor(private route: ActivatedRoute, private spotifyService: SpotifyService) { }
 
   ngOnInit() {
   	this.albumId = this.route.snapshot.paramMap.get('id');
   	//TODO: inject spotifyService and use it to get the album data and the tracks for the album
+    this.spotifyService.getAlbum(this.albumId).then((data) => {this.album = data;});
+    this.spotifyService.getTracksForAlbum(this.albumId).then((data) => { this.tracks = data;});
   }
 
 }
